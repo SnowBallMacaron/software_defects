ef1eeee735451c6e31d3f299be6df3a0072b74f5
email@example.com <email@example.com>
2023-10-14 23:01:06 +0100

    niuniunie

--- preprocess.py
+++ preprocess.py
@@ -1,24 +1,29 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import imblearn
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
 
 from sklearn.feature_selection import RFE, SelectFromModel, SelectKBest, f_regression, SequentialFeatureSelector
 from sklearn.linear_model import LinearRegression, Lasso, LogisticRegression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingClassifier
 from sklearn.model_selection import StratifiedKFold, train_test_split
-
+import matplotlib.pyplot as plt
 
 # RFE method model = LinearRegression()
-def rfeFeatureSelect(model, X, y):
+def rfeFeatureSelect(model, data, n_features_to_select):
 
     rfe = RFE(
-        estimator=model
-        # n_features_to_select = 19
+        estimator=model,
+        n_features_to_select=n_features_to_select
     )
-    feature_names = X.drop(columns='kfold').columns
-    rfe = rfe.fit(X.drop(columns='kfold'), y)
+    feature_names = data.drop(columns=['defects']).columns
+    rfe = rfe.fit(data.drop(columns=['defects']), data['defects'])
     support = rfe.get_support()
-    selected_feature_rfe = [x for x, y in zip(feature_names, support) if y == True] + ['kfold']
-    X = X[selected_feature_rfe]
-    return X, selected_feature_rfe
+    selected_feature_rfe = [x for x, y in zip(feature_names, support) if y == True] + ['defects']
+    data = data[selected_feature_rfe]
+    return data, selected_feature_rfe
 
 
 ## Select from model method
@@ -37,16 +42,16 @@ def rfeFeatureSelect(model, X, y):
 # print(sfm.threshold_)
 
 #wrapper method
-def seqFeatureSelect(model, data, direction, scoring):
+def seqFeatureSelect(model, data, direction, scoring, n_features_to_select):
     sfs = SequentialFeatureSelector(model,
-                                    # n_features_to_select=19,
+                                    n_features_to_select=n_features_to_select,
                                     direction=direction,
                                     scoring=scoring)
     # fit the object to the training data
-    feature_names = data.drop(columns=['kfold', 'defects']).columns
-    sfs = sfs.fit(data.drop(columns=['kfold', 'defects']), data['defects'])
+    feature_names = data.drop(columns=['defects']).columns
+    sfs = sfs.fit(data.drop(columns=['defects']), data['defects'])
     support = sfs.get_support()
-    selected_feature = [x for x, y in zip(feature_names, support) if y == True] + ['kfold', 'defects']
+    selected_feature = [x for x, y in zip(feature_names, support) if y == True] + ['defects']
     data = data[selected_feature]
     return data, selected_feature
 
@@ -67,17 +72,51 @@ def create_folds(data, n_splits, seed=None):
 #                    'defects': [1, 1, 1, 1, 1, 1]})
 # print(create_folds(df, 3, 4))
 
+def outlier(data, factor):
+    no_outliers = data.copy()
+    metrics = data.describe()
+    for i in data.columns:
+        if i != "defects":
+            Q1 = metrics[i]["25%"]
+            Q3 = metrics[i]["75%"]
+            IQR = Q3 - Q1
+            upper = Q3 + IQR * factor
+            lower = Q1 - IQR * factor
+            no_outliers.loc[no_outliers[i] > upper, i] = np.NaN
+            no_outliers.loc[no_outliers[i] < lower, i] = np.NaN
+
+    no_outliers.dropna(inplace=True)
+    no_outliers.reset_index(drop=True, inplace=True)
+    return no_outliers
+
+
 if __name__ == '__main__' :
     raw_data = pd.read_csv('./train.csv')
     # X = raw_data.iloc[:,:-1]
     # y = raw_data['defects'].astype(int)
     direction = 'backward'
     scoring = 'roc_auc'
-    model = Lasso()
+    model = Lasso()  # LinearRegression Lasso
 
-    data = create_folds(raw_data, n_splits=5, seed=1)
-    data['defects'] = data['defects'].astype(int)
-    print(data.groupby(['kfold','defects']).size())
-
-    X, selected_features = seqFeatureSelect(model, data, direction, scoring)
+    data, selected_features = seqFeatureSelect(model, raw_data, direction, scoring)
     print(selected_features)
+
+    data = outlier(data)
+
+    data = create_folds(data, n_splits=5, seed=1)
+    data['defects'] = data['defects'].astype(int)
+    print(data.groupby(['kfold', 'defects']).size())
+
+
+    # plt.plot(figsize=(10, 3), dpi=200)
+    # sns.countplot(data, y="defects", hue="defects")
+    # plt.show()
+def sampling(data, strategy, method): ## under, over
+    if method == 'under':
+        sample = RandomUnderSampler(sampling_strategy=strategy)
+    else:
+        sample = RandomOverSampler(sampling_strategy=strategy)
+    X, y = sample.fit_resample(data.drop(columns=['defects']), data['defects'])
+    data = pd.concat([X, y], axis=1).reset_index(drop=True)
+    return data
+
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
-from preprocess import rfeFeatureSelect, create_folds, seqFeatureSelect
+from preprocess import rfeFeatureSelect, create_folds, seqFeatureSelect, outlier, sampling
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression, Lasso, LogisticRegression
 from sklearn import metrics
 import xgboost as xgb
@@ -19,6 +20,22 @@ def modelSelect(data, model, k=5):
 
         print(f"Fold = {fold}, AUC = {auc}")
 
+def train_model(data, ):
+    direction = 'backward'
+    scoring = 'roc_auc'
+    xgb_model = xgb.XGBClassifier(
+        n_estimators=100,
+        n_jobs=-1,
+        max_depth=10
+    )
+    train_model = xgb_model
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     raw_data = pd.read_csv('./train.csv')
@@ -26,18 +43,41 @@ if __name__ == "__main__":
     # y = raw_data['defects'].astype(int)
     direction = 'backward'
     scoring = 'roc_auc'
-    feature_model = Lasso()
-    train_model = xgb.XGBClassifier(
-        n_estimators=200,
+
+    xgb_model = xgb.XGBClassifier(
+        n_estimators=100,
         n_jobs=-1,
-        max_depth=3
+        max_depth=10
     )
+    lasso_model = Lasso()
+    linear_model = LinearRegression()
 
-    data = create_folds(raw_data, n_splits=5, seed=1)
-    data['defects'] = data['defects'].astype(int)
-    print(data.groupby(['kfold','defects']).size())
+    train_model = xgb_model
+    feature_model = lasso_model
+    n_features_to_select = 13
+    factor = 10
 
-    data, selected_features = seqFeatureSelect(feature_model, data, direction, scoring)
+    print('selecting features')
+    data, selected_features = seqFeatureSelect(feature_model, raw_data, direction, scoring, n_features_to_select)
+    #data, selected_features = rfeFeatureSelect(feature_model, raw_data, 19)
     print(selected_features)
 
-    modelSelect(data, train_model)
+    # print('removing outliers')
+    # data = outlier(data, factor=factor)
+
+    print('sampling')
+    sample_under1 = sampling(data, 'majority', 'under')
+    sample_under2 = sampling(data, 1/3, 'under')
+    sample_under3 = sampling(data, 0.5, 'under')
+    sample_over1 = sampling(data, 'minority', 'over')
+    sample_over2 = sampling(data, 1/3, 'over')
+    sample_over3 = sampling(data, 0.5, 'over')
+
+
+    samples = [data, sample_under1, sample_under2, sample_under3, sample_over1, sample_over2, sample_over3]
+    for i, sample in enumerate(samples):
+        print(f'----------sample{i+1}------------')
+        data = create_folds(sample, n_splits=5, seed=1)
+        data['defects'] = data['defects'].astype(int)
+        # print(data.groupby(['kfold', 'defects']).size())
+        modelSelect(data, train_model)
